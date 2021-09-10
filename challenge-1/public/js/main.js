b0ba2cf3af08f90540a12a71b1e3fb01a8c5da2f
l3onardodev.github.io <leonardo.contato03@gmail.com>
2021-09-10 15:35:19 -0300

    Favorite post featurevisually  done.

--- challenge-1/public/js/main.js
+++ challenge-1/public/js/main.js
@@ -1,6 +1,8 @@
 const searchInput = document.querySelector('.input-search-bar');
 const searchContainer = document.querySelector('.search-bar-container');
 
+const favIcon = document.querySelector('.fav-icon');
+
 //todo: maybe this is not the perfect solution.
 document.addEventListener('click', () => {
     if (searchInput === document.activeElement) {
@@ -8,4 +10,18 @@ document.addEventListener('click', () => {
     } else {
         searchContainer.style.boxShadow = 'none';
     }
+})
+
+favIcon.addEventListener('mouseover', (element) => {
+    if (favIcon.classList.contains('favorite')) return;
+    favIcon.src = 'images/fav-icon-filled.png';
+});
+
+favIcon.addEventListener('mouseleave', () => {
+    if (favIcon.classList.contains('favorite')) return;
+    favIcon.src = 'images/fav-icon.png';
+})
+
+favIcon.addEventListener('click', () => {
+    favIcon.classList.contains('favorite') ? favIcon.classList.remove('favorite') : favIcon.classList.add('favorite');
 })
