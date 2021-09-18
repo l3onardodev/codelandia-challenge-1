1c56cd90bea0b2e3c444fffb9282fa54be6cc494
l3onardodev.github.io <leonardo.contato03@gmail.com>
2021-09-17 22:26:51 -0300

    Improvements in the UI; New's image added beside news text

--- challenge-1/public/js/main.js
+++ challenge-1/public/js/main.js
@@ -1,44 +1,48 @@
-const searchInput = document.querySelector('.input-search-bar');
-const searchContainer = document.querySelector('.search-bar-container');
+const searchInput = document.querySelector(".input-search-bar");
+const searchContainer = document.querySelector(".search-bar-container");
 
-const favIcon = document.querySelector('.fav-icon');
+const favIcon = document.querySelector(".fav-icon");
 
 //modal feature
-const buttonCreatePost = document.querySelector('.button-create-post');
-const modal = document.querySelector('.modal');
-const modalLowOpacity = document.querySelector('.modal-low-opacity');
+const buttonCreatePost = document.querySelector(".button-create-post");
+const modal = document.querySelector(".modal");
+const modalLowOpacity = document.querySelector(".modal-low-opacity");
 
-const closeModalButton = document.querySelector('.modal-close-icon');
+const closeModalButton = document.querySelector(".modal-close-icon");
 
 //todo: maybe this is not the perfect solution.
-document.addEventListener('click', () => {
+document.addEventListener("click", () => {
     if (searchInput === document.activeElement) {
-        searchContainer.style.boxShadow = '0px 0px 15px rgb(0, 0, 0, 0.2)';
+        searchContainer.style.boxShadow = "0px 0px 15px rgb(0, 0, 0, 0.2)";
     } else {
-        searchContainer.style.boxShadow = 'none';
+        searchContainer.style.boxShadow = "none";
     }
-})
-
-favIcon.addEventListener('mouseover', (element) => {
-    if (favIcon.classList.contains('favorite')) return;
-    favIcon.src = 'images/fav-icon-filled.png';
 });
 
-favIcon.addEventListener('mouseleave', () => {
-    if (favIcon.classList.contains('favorite')) return;
-    favIcon.src = 'images/fav-icon.png';
-})
-
-favIcon.addEventListener('click', () => {
-    favIcon.classList.contains('favorite') ? favIcon.classList.remove('favorite') : favIcon.classList.add('favorite');
+favIcon.addEventListener("mouseover", (element) => {
+    if (favIcon.classList.contains("favorite")) return;
+    favIcon.src = "images/fav-icon-filled.png";
+    favIcon.style.transform = "scale(1.3)";
 });
 
-buttonCreatePost.addEventListener('click', function() {
-    modal.classList.remove('hidden');
-    modalLowOpacity.classList.remove('hidden');
+favIcon.addEventListener("mouseleave", () => {
+    if (favIcon.classList.contains("favorite")) return;
+    favIcon.src = "images/fav-icon.png";
+    favIcon.style.transform = "scale(1)";
 });
 
-closeModalButton.addEventListener('click', function() {
-    modal.classList.toggle('hidden');
-    modalLowOpacity.classList.toggle('hidden');
-})
+favIcon.addEventListener("click", () => {
+    favIcon.classList.contains("favorite")
+        ? favIcon.classList.remove("favorite")
+        : favIcon.classList.add("favorite");
+});
+
+buttonCreatePost.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    modalLowOpacity.classList.remove("hidden");
+});
+
+closeModalButton.addEventListener("click", function () {
+    modal.classList.toggle("hidden");
+    modalLowOpacity.classList.toggle("hidden");
+});
